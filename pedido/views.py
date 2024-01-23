36b830a7c58047c161e18e54a2e0df4f459b1b64
Caue <crburgardt@gmail.com>
2024-01-23 16:02:52 -0300

    fix views

--- pedido/views.py
+++ pedido/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
+from django.http import HttpResponse
+
 
 
 class Pagar(View):
--- produto/urls.py
+++ produto/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = 'produto'  #produto:detalhe
 
 urlpatterns = [
-    path('', views.ListaProduto.as_view(), name="lista"),
-    path('<slug>', views.DetalheProduto.as_view(), name="detalhe"),
+    path('', views.ListaProdutos.as_view(), name="lista"),
+    path('<slug>', views.DetalheProdutos.as_view(), name="detalhe"),
     path('adicionar/', views.AdicionarProduto.as_view(), name="adicionar"),
     path('remover/', views.RemoverProduto.as_view(), name="remover"),
     path('carrinho/', views.Carrinho.as_view(), name="carrinho"),
