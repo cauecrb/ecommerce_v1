2cd74f7008fedd27b3cef76f6af7ed5665b8b5d1
Caue <crburgardt@gmail.com>
2024-01-24 16:43:10 -0300

    redirect and initial create carrinho

--- produto/views.py
+++ produto/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 
@@ -24,9 +25,37 @@ class DetalheProdutos(DetailView):
 
 
 class AdicionarProduto(View):
-    #pass
     def get(self, *args, **kwargs):
-        return HttpResponse('adicionar produto')
+        #http_referer = self.request.META['HTTP_REFERER']
+        http_referer = self.request.META.get(
+            'HTTP_REFERER',
+            reverse('produto:lista')
+        )
+        variacao_id = self.request.GET.get('vid')
+        
+        if not variacao_id:
+            messages.error(
+                self.request, 'Produto Inexistente'
+            )
+            return redirect(http_referer)
+        
+        variacao = get_object_or_404(models.Variacao, pk=variacao_id)
+        # TODO: redirecionar caso 404
+        
+        if not self.request.session.get('carrinho'):
+            self.request.session['carrinho'] = {}
+            self.request.session.save()
+        
+        carrinho = self.request.session['carrinho']
+        
+        if variacao_id in carrinho:
+            # TODO: Variação existe no carrinho
+            pass
+        else:
+            # TODO: Variação não existe no carrinho
+            pass
+        
+        return HttpResponse(f'{variacao.produto} {variacao.nome}')
 
 class RemoverProduto(View):
     #pass
