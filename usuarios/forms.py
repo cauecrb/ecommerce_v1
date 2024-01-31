bd930a51ea301933e871869c5b70964fcdf9ed93
Caue <crburgardt@gmail.com>
2024-01-31 17:50:52 -0300

    input password, confirm usuario is logado

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -9,10 +9,38 @@ class UsuariosForm(forms.ModelForm):
         exclude = ('usuario',)
 
 class UserForm(forms.ModelForm):
+    password = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Senha'
+    )
+    
+    password2 = forms.CharField(
+        required=False,
+        widget=forms.PasswordInput(),
+        label='Repita sua senha'
+    )
+    
+    
+    def __init__(self, usuario=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.usuario = usuario
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'username','email', 'password')
+        fields = ('first_name', 'last_name', 'username','email', 'password', 'password2')
         
     def clean(self, *args, **kwargs):
         data = self.data
         cleaned = self.cleaned_data
+        validation_error_msgs = {}
+        
+        #usuários logados: atualizar usuário
+        if self.usuario:
+            print('LOGADO')
+        #usuários não logados: cadastrar
+        else:
+            pass
+        
+        if validation_error_msgs:
+            raise(forms.ValidationError(validation_error_msgs))
--- usuarios/views.py
+++ usuarios/views.py
@@ -14,10 +14,21 @@ class BaseUsusarios(View):
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
         
-        self.contexto = {
-            'userform': forms.UserForm(data=self.request.POST or None),
-            'usuariosform': forms.UsuariosForm(data=self.request.POST or None)
-        }
+        if self.request.user.is_authenticated:
+            self.contexto = {
+                'userform': forms.UserForm(
+                    data=self.request.POST or None,
+                    usuario=self.request.user,
+                    instance=self.request.user,
+                ),
+                'usuariosform': forms.UsuariosForm(data=self.request.POST or None)
+            }
+        else:
+                        self.contexto = {
+                'userform': forms.UserForm(data=self.request.POST or None),
+                'usuariosform': forms.UsuariosForm(data=self.request.POST or None)
+            }
+        
         
         self.renderizar = render(self.request, self.template_name, self.contexto)
     
