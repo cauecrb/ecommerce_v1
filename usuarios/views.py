cf06642f578cde85749c170053996ed4f6cca65a
Caue <crburgardt@gmail.com>
2024-02-05 17:38:11 -0300

    view user loged or register, part 2

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+import copy
 
 from . import models
 from . import forms
@@ -13,6 +15,8 @@ class BaseUsusarios(View):
     
     def setup(self, *args, **kwargs):
         super().setup(*args, **kwargs)
+        
+        self.carrinho = copy.deepcopy(self.request.get('carrinho', {}))
         
         self.usuarios = None
                 
@@ -47,14 +51,45 @@ class BaseUsusarios(View):
 
 class Criar(BaseUsusarios):
     def post(self, *args, **kwargs):
-        if not self.userform.is_valid() or not self.usuariosform.is_valid():
+        #if not self.userform.is_valid() or not self.usuariosform.is_valid():
+        if not self.userform.is_valid():
             print('Invalido')
             return self.renderizar
+        
+        username = self.userform.cleaned_data.get('username')
+        password = self.userform.cleaned_data.get('password')
+        email = self.userform.cleaned_data.get('email')
+        first_name = self.userform.cleaned_data.get('first_name')
+        last_name = self.userform.cleaned_data.get('last_name')
+
+        #usuario logado
         if self.request.user.is_authenticated:
-            pass
+            usuario = get_object_or_404(User, username=self.request.user.username)
+            
+            usuario.username = username
+            
+            if password:
+                usuario.set_password(password)
+            
+            usuario.email = email
+            usuario.first_name = first_name
+            usuario.last_name = last_name
+            usuario.save()
+        
+        #usuario não logado
         else:
             pass
+            usuario = self.userform.save(commit=False)
+            usuario.set_password(password)
+            usuario.save()
             
+            perfil = self.userform.save(commit=False)
+            perfil.usuario = usuario
+            perfil.save()
+        
+        self.request.session['carrinho'] = self.carrinho
+        self.request.session.save()
+        
         return self.renderizar
 
 
